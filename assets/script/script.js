ff75ed883a18fae20b248268f048fc6f0c9b080f
lucas-araujo-96 <matheusaraujo.lucas@gmail.com>
2021-06-22 14:11:14 -0300

    Persistência local adicionada 22-06

--- assets/script/script.js
+++ assets/script/script.js
@@ -17,6 +17,7 @@ function escopo() {
     function criaNode(texto) {
         let node = document.createElement(`li`);
         let textoNode = document.createTextNode(texto);
+        node.classList.add(`item`);
         node.appendChild(textoNode);
         node.appendChild(criaBotaoDeletar());
         return node;
@@ -27,18 +28,45 @@ function escopo() {
         document.querySelector(`.lstTarefas`).appendChild(node);
     };
 
+    //função que atualiza o localStorage
+    function atualStorage() {
+        let lista = document.querySelectorAll(`.item`);
+        let arrLista = [];
+        for (let i of lista) {
+            arrLista.push(i.innerText);
+        };
+        arrLista = JSON.stringify(arrLista);
+        localStorage.setItem(`listaDeTarefas`, arrLista);
+    };
+
+    //função que preenche a lista ao carregar a página
+    function puxaStorage() {
+        let lista = localStorage.getItem(`listaDeTarefas`);
+        lista = JSON.parse(lista);
+        for (let i of lista) {
+            document.querySelector(`.lstTarefas`).appendChild(criaNode(i));
+        };
+    };
+
     function main() {
-        //Função que, ao se clicar no botão de inserir tarefa, cria um novo item na lista de tarefas
+        //EventListener que, ao se clicar no botão de inserir tarefa, cria um novo item na lista de tarefas
+        //e atualiza o localStorage
         document.querySelector(`.btnInsereTarefa`).addEventListener(`click`, function() {
             insereNode(criaNode(pegaTexto()));
+            atualStorage();
         });
 
         //função que apaga o li quando seu botão de deletar é clicado
+        //e atualiza o localStorage
         document.addEventListener(`click`, function(e) {
             if (e.target.classList.contains(`deletar`)) {
                 e.target.parentElement.remove();
+                atualStorage();
             }
         });
+
+        //atualiza a página com o localStorage quando ela é aberta
+        puxaStorage();
     }; 
     main();
 };
