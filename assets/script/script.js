8955cee610063bd8c83d1c3823c93d1307691fd5
lucas-araujo-96 <matheusaraujo.lucas@gmail.com>
2021-06-28 16:49:56 -0300

    Funcionalidade de riscar itens adicionada 28-06

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,7 +4,7 @@ function escopo() {
         return document.querySelector(`.txtTarefa`).value;
     };
 
-    //função que cria um botao com valor `remover` e classe `deletar`
+    //função que cria um node botao com valor `remover` e classe `deletar`
     function criaBotaoDeletar() {
         let btnDel = document.createElement(`input`);
         btnDel.setAttribute(`type`, `button`);
@@ -14,15 +14,35 @@ function escopo() {
         return btnDel;
     };
 
-    //função que gera um node li com o texto do campo de tarefas e um botão de remover
+    //função que cria um node checkbox com a classe `riscar`
+    function criaCheckBox() {
+        let cb = document.createElement(`input`);
+        cb.setAttribute(`type`, `checkbox`);
+        cb.classList.add(`riscar`);
+        return cb;
+    };
+
+    //função que gera um node li com o texto do campo de tarefas, um botão de remover e um checkbox que "risca" a tarefa
     function criaNode(texto) {
         let node = document.createElement(`li`);
         let textoNode = document.createTextNode(texto);
+        let cb = criaCheckBox();
         node.classList.add(`item`);
+        node.appendChild(cb);
         node.appendChild(textoNode);
         node.appendChild(criaBotaoDeletar());
         return node;
     };
+
+    //função que checa se o li está riscado e altera uma classe de acordo
+    function riscaItem(item) {
+        if (!item.classList.contains(`riscado`)) {
+            item.classList.add(`riscado`);
+        } else {
+            item.classList.remove(`riscado`);
+        }
+        
+    }; 
 
     //função que insere um node <li> formado no ul da página
     function insereNode(node) {
@@ -50,19 +70,22 @@ function escopo() {
     };
 
     function main() {
-        //EventListener que, ao se clicar no botão de inserir tarefa, cria um novo item na lista de tarefas
-        //e atualiza o localStorage
-        document.querySelector(`.btnInsereTarefa`).addEventListener(`click`, function() {
+
+        //EventListener para criar item/atualizar localStorage ao se pressionar enter no campo do form
+        document.querySelector(`.formulario`).addEventListener(`submit`, function() {
             insereNode(criaNode(pegaTexto()));
             atualStorage();
         });
 
         //função que apaga o li quando seu botão de deletar é clicado
         //e atualiza o localStorage
+        //e que risca o item ao se clicar na checkbox
         document.addEventListener(`click`, function(e) {
             if (e.target.classList.contains(`deletar`)) {
                 e.target.parentElement.remove();
                 atualStorage();
+            } else if (e.target.classList.contains(`riscar`)) {
+                riscaItem(e.target.parentElement);
             }
         });
 
